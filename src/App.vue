94217552cbd0f366a8df62a476107efeb03b5199
pepe-gith <majocavi@gmail.com>
2024-04-29 21:35:38 +0200

    cambio .2 en app.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Voy ha hacer cambio 2ª vez 1</h1>
+  <ComputedExample />
+  
+</template>
+
+<script lang="ts" setup>
+  import ComputedExample  from './components/ComputedExample'
+  
+</script>
+
+<style>
+h1
+ {
+   color :red
+ }
+ 
+</style>
+
+
